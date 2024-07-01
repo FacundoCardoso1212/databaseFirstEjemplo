e7df1eee1169cbbffdf74f6d6efc4e4d32ec58cb
FacundoCardoso1212 <facundoocardoso121201@gmail.com>
2024-07-01 01:00:23 -0300

    Agregar archivos de proyecto.

---
+++ databaseFirstEjemplo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace databaseFirstEjemplo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (databaseFirstEntities db = new databaseFirstEntities())
+            {
+                var lst = db.Estudiante;
+
+                foreach (var alumno in lst)
+                {
+                    Console.WriteLine($"ID: {alumno.id} Nombre: {alumno.nombre} Apellido: {alumno.apellido} Edad: {alumno.edad}");
+                }
+
+                Console.WriteLine("Estudiantes de 22 años o mas.");
+
+                var estudiantesMayoresDe22 = db.Estudiante
+                    .Where(alumno => alumno.edad >= 22)
+                    .OrderBy(alumno => alumno.nombre)
+                    .ToList();
+
+                foreach (var alumno in estudiantesMayoresDe22)
+                {
+                    Console.WriteLine($"ID: {alumno.id} Nombre: {alumno.nombre} Apellido: {alumno.apellido} Edad: {alumno.edad}");
+
+                }
+
+                Console.ReadLine();
+
+            }
+        }
+    }
+}
